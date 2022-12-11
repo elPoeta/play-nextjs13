eef0c298d280f60b32d66e0f753651e9787ffd48
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-12-11 18:30:02 -0300

    refactor add services to fetch data

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
+import { getCharacter } from '../../../services/fetchingAPI';
 import { Character } from '../../../types/character';
-import { BASE_API_URL } from '../../../utils/constants';
 
-const fetchCharacter = async ({ id }: { id: number }) => {
-  const response = await fetch(`${BASE_API_URL}/character/${id}`);
-  const json: Character = await response.json();
-  return json;
-}
 
 const CharacterPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   //console.log(id); //{ params: { id: '34'}, searchParams: {  } }
-  const character: Character = await fetchCharacter({ id: parseInt(id) });
+  const character: Character = await getCharacter({ id: parseInt(id) });
   return (
     <div>Character ID {id}
       <pre>{JSON.stringify(character, null, 2)}</pre>
