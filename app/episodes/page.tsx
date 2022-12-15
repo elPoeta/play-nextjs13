05956d8d07adc67987f9d1814be68f628fafab4f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-12-15 08:18:10 -0300

    episodes pagination

--- app/episodes/page.tsx
+++ app/episodes/page.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Loader } from '../../components/common/Loader';
+import { Episodes } from '../../components/episode/Episodes';
+import { Pagination } from '../../components/pagination/Pagination';
 import { getDataByPage } from '../../services/fetchingAPI';
 import { APIResponse } from '../../types/apiType';
 import { Episode } from '../../types/episode';
+
 
 const EpisodePage =  async ({ searchParams }: { searchParams: { page?: string } }) => {
   const { page = '1' } = searchParams;
   const { info, results }: APIResponse = await getDataByPage({ page, path: 'episode' });
   const episodes = results as Episode[];
   return (
-    <div>
-      <pre>{JSON.stringify(episodes,null,2)}</pre>
+    <Suspense fallback={<Loader />}>
+    <div style={{padding: '4rem'}}>
+      <Episodes episodes={episodes} />
+      <Pagination info={info} page={parseInt(page)} route="episodes" />
     </div>
+    </Suspense>
   )
 }
 
--- components/episode/Episodes.tsx
+++ components/episode/Episodes.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Episode } from '../../types/episode';
 import styles from './episodes.module.css';
+import { EpisodesItem } from './EpisodesItem';
 
-export const Episodes = () => {
+export const Episodes = ({episodes}:{episodes: Episode[]}) => {
   return (
     <div className={styles.wrapper}>
-      Episodes
-    </div>
+    {episodes.map(episode => 
+      <div key={episode.id} >
+        <EpisodesItem episode={episode}/>
+      </div>
+    )}
+ </div>
   )
 }
